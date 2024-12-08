386c812784c58cbb6d20e7ec8de187310ed01fff
Friedrich Chen <friedrichchen@Inuyasha.local>
2024-12-08 12:53:19 +0800

    feat: overwrite

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,18 @@ use console::style;
 use rusqlite::{Connection, Result};
 use std::fs;
 use std::path::Path;
+use std::path::PathBuf;
 
 #[derive(Debug)]
-pub struct TodoList {
+pub struct Todo {
     pub id: i32,
     pub name: String,
     pub date_added: String,
     pub is_done: u8,
 }
-
-impl TodoList {
+impl Todo {
     pub fn new(id: i32, name: String, date_added: String, is_done: u8) -> Self {
-        TodoList {
+        Todo {
             id,
             name,
             date_added,
@@ -22,49 +22,83 @@ impl TodoList {
     }
 
     pub fn add(conn: &Connection, name: &str) -> Result<()> {
-        conn.execute("INSERT INTO todolist (name) VALUES (?)", &[name])?;
+        conn.execute("INSERT INTO todo (name) VALUES (?)", &[name])?;
         Ok(())
     }
 
-    pub fn list(conn: &Connection, sort_by_id: bool) -> Result<Vec<TodoList>> {
-        let sql = if sort_by_id {
-            "SELECT * FROM todolist BY id"
+    pub fn list(conn: &Connection, sort_by_status: bool) -> Result<Vec<Todo>> {
+        let sql = if sort_by_status {
+            "SELECT * FROM todo ORDER BY is_done, id"
         } else {
-            "SELECT * FROM todolist BY is_done, id"
+            "SELECT * FROM todo ORDER BY id"
         };
         let mut stmt = conn.prepare(sql)?;
         let todo_iter = stmt.query_map((), |row| {
-            Ok(TodoList::new(
+            Ok(Todo::new(
                 row.get(0)?,
                 row.get(1)?,
                 row.get(2)?,
                 row.get(3)?,
             ))
         })?;
-        let mut todo_item = Vec::new();
-        for item in todo_iter {
-            todo_item.push(item?);
+
+        let mut todos = Vec::new();
+        for todo in todo_iter {
+            todos.push(todo?);
         }
-        Ok(todo_item)
+        Ok(todos)
     }
 
-    pub fn print_list(todo_list: Vec<TodoList>) -> Result<()> {
-        for todo_item in todo_list {
-            let status = if todo_item.is_done == 1 {
+    pub fn print_list(todos: Vec<Todo>) -> Result<()> {
+        for todo in todos {
+            let status = if todo.is_done == 1 {
                 style("Done").green()
             } else {
                 style("Pending").red()
             };
-            print!(
+            println!(
                 "{:>4} | {:<44} {:<8} {}",
-                style(todo_item.id).cyan().bright(),
-                style(truncate_at(&todo_item.name, 44)).bright(),
+                style(todo.id).cyan().bright(),
+                style(truncate_at(&todo.name, 44)).bright(),
                 status,
-                style(todo_item.date_added).dim(),
-            )
+                style(todo.date_added).dim(),
+            );
         }
         Ok(())
     }
+}
+
+pub fn get_connection() -> Result<Connection> {
+    let db_folder = "./todo_db/".to_string();
+    let db_file_path = format!("{}{}", db_folder, "todo.sqlite");
+    verify_db_path(&db_folder)?;
+    let conn = Connection::open(db_file_path)?;
+    verify_db(&conn)?;
+    Ok(conn)
+}
+
+pub fn verify_db_path(db_folder: &str) -> Result<()> {
+    if !Path::new(db_folder).exists() {
+        match fs::create_dir(db_folder) {
+            Ok(_) => println!("Folder '{}' created.", db_folder),
+            Err(err) => eprintln!("Error creating folder: {}", err),
+        }
+    }
+    Ok(())
+}
+
+pub fn verify_db(conn: &Connection) -> Result<()> {
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS todo (
+                id INTEGER NOT NULL,
+                name TEXT NOT NULL,
+                date_added REAL NOT NULL DEFAULT current_timestamp,
+                is_done NUMERIC NOT NULL DEFAULT 0,
+                PRIMARY KEY(id AUTOINCREMENT)
+            )",
+        [],
+    )?;
+    Ok(())
 }
 
 pub fn truncate_at(input: &str, max: i32) -> String {
@@ -78,51 +112,12 @@ pub fn truncate_at(input: &str, max: i32) -> String {
 }
 
 pub fn help() -> Result<()> {
-    let help_title = "\nAvailable commands";
-    let help_text = r#"    
-        - add [TASK]
-            Ads new task/s
-            Example: tbr add "take a shower"
-
-        - list
-            Lists all tasks
-            Example: tbr list
-    "#;
+    let help_title = "\nAvailable commands:";
+    let help_text = r#"
+            - add [TASK]
+                add new task/s
+        "#;
     println!("{}", style(help_title).cyan().bright());
     println!("{}", style(help_text).green());
-    Ok(())
-}
-
-pub fn get_connection() -> Result<Connection> {
-    let db_folder = "./todolist_db/".to_string();
-    let db_file_path = db_folder.clone() + "todolist.sqlite";
-    verify_db_path(&db_folder)?;
-
-    let conn = Connection::open(db_file_path)?;
-    verify_db(&conn)?;
-    Ok(conn)
-}
-
-pub fn verify_db(conn: &Connection) -> Result<()> {
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS todo (
-    	id	        INTEGER NOT NULL,
-    	name	    TEXT NOT NULL,
-    	date_added	REAL NOT NULL DEFAULT current_timestamp,
-    	is_done	    NUMERIC NOT NULL DEFAULT 0,
-    	    PRIMARY KEY(id AUTOINCREMENT)
-    )",
-        [],
-    )?;
-    Ok(())
-}
-
-pub fn verify_db_path(db_folder: &str) -> Result<()> {
-    if !Path::new(db_folder).exists() {
-        match fs::create_dir(db_folder) {
-            Ok(_) => println!("folder '{}' created.", db_folder),
-            Err(e) => eprintln!("Error creating folder: {}", e),
-        }
-    }
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,34 @@
 use rusqlite::Result;
 use std::env;
-use todolist_by_rust::{get_connection, help, TodoList};
+use todolist_by_rust::*;
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
-        help()?;
         std::process::exit(1);
     }
 
     let conn = get_connection()?;
+
     let command = &args[1];
-    let remaining = &args[2..].iter().cloned().collect::<Vec<_>>().join(" ");
+    let remaining: &String = &args[2..].iter().cloned().collect::<Vec<_>>().join(" ");
 
     match command.as_str() {
         "add" => {
             if remaining.as_str().is_empty() {
-                help()?;
                 std::process::exit(1);
             } else {
-                TodoList::add(&conn, remaining.as_str())?;
+                Todo::add(&conn, remaining.as_str())?;
             }
             Ok(())
         }
         "list" => {
             println!("TODO List (sorted by id):");
-            let todos = TodoList::list(&conn, false)?;
-            TodoList::print_list(todos)?;
+            let todos = Todo::list(&conn, false)?;
+            Todo::print_list(todos)?;
             Ok(())
         }
+        "help" | "--help" | "-h" | _ => help(),
     }?;
 
     Ok(())
